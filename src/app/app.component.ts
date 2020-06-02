9a5152e9c9c2e61b90a07ccf0d8ad53cf8e713a1
Marlon-Santos <marlonergondossantos@gmail.com>
2020-06-02 17:35:00 -0300

    create observable example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RxJs-App';
+  test = new Observable(() => {
+    console.log(Math.ceil(Math.random() * 50));
+  });
+  ngOnInit(): void {
+    this.test.subscribe((observer) => observer);
+  }
 }
