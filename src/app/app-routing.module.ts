89594279bd7cac0c8a390a3acf4356cc19144651
Marlon-Santos <marlonergondossantos@gmail.com>
2020-06-03 15:54:34 -0300

    create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestxComponent } from './testx/testx.component';
+import { TestyComponent } from './testy/testy.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'testx',
+    component: TestxComponent,
+  },
+  {
+    path: 'testy',
+    component: TestyComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
