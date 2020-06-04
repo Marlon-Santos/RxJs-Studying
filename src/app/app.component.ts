3e14dd04ee99d118a6e0e66b7d009a4f74a53bd9
Marlon-Santos <marlonergondossantos@gmail.com>
2020-06-04 00:10:03 -0300

    create generators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subject, range, from, interval } from 'rxjs';
-import { publish, timeout } from 'rxjs/operators';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  @ViewChild('input') myInput: ElementRef<HTMLInputElement>;
+  generator;
+  count = [10, 20, 30, 40, 50];
+  ngOnInit(): void {
+    function* test(test: number[]) {
+      yield* test;
+    }
+    this.generator = test(this.count);
+  }
+  onChange(): void {
+    console.log(this.myInput.nativeElement.value);
+    console.log(this.generator.next());
+  }
+}
